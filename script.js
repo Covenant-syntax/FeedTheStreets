1cd56a3987edefa68868b031ec37f3004f535386
Covenant-syntax <covenantchris4christ@gmail.com>
2024-09-27 09:38:26 +0100

    Final commit for this project

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 // custom js 
 
+            // Our sponsors
+
+            // const swiper = new Swiper('.swiper', {
+            //     // Optional parameters
+            //     direction: 'horizontal',
+            //     loop: true,
+            
+            //     // If we need pagination
+            //     pagination: {
+            //     el: '.swiper-pagination',
+            //     },
+            
+            //     // Navigation arrows
+            //     navigation: {
+            //     nextEl: '.swiper-button-next',
+            //     prevEl: '.swiper-button-prev',
+            //     },
+            
+            //     // And if we need scrollbar
+            //     scrollbar: {
+            //     el: '.swiper-scrollbar',
+            //     },
+            // });
+
             // mobile menu
 
                 function toggleMenu() {
@@ -206,6 +230,5 @@
         };
 
             
-    
-
+        
         
